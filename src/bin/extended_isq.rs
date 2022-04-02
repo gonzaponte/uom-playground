de8249876738cf0ed24e3709d82f27199d1c9797
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2022-04-02 13:18:22 +0300

    Add working example of parallel systems of quantities

--- src/bin/extended_isq.rs
+++ src/bin/extended_isq.rs
@@ -5,167 +5,150 @@ use std::f64::consts::PI;
 
 use uom::fmt::DisplayStyle::Abbreviation;
 
-pub use uom::si::{self, *};
+use crate::digit::{adc, kadc};
+use crate::count::{pes, kpes};
 
-// use uom::si::{ length
-//              , mass
-//              , time
-//              , electric_current
-//              , thermodynamic_temperature
-//              , amount_of_substance
-//              , luminous_intensity
-//              };
+use uom::si::f64::{Length, Time, Ratio};
+use crate::f64::{Digit, Count};
 
-// use uom::si::length::meter;
-// use uom::si::mass::kilogram;
-// use uom::si::time::second;
-// use uom::si::electric_current::ampere;
-// use uom::si::thermodynamic_temperature::kelvin;
-// use uom::si::amount_of_substance::mole;
-// use uom::si::luminous_intensity::candela;
+use uom::si::length::millimeter;
+use uom::si::time  ::nanosecond;
 
-use crate::digitized_signal::{adc, kadc};
-use crate::calibrated_signal::{pes, kpes};
+// #[allow(non_camel_case_types)]
+// struct adc(uom::si::f64::Ratio);
+// struct pes(uom::si::f64::Ratio);
 
 
-// Define a new quantity
 #[macro_use]
-pub mod digitized_signal {
+mod digit {
     quantity! {
-        quantity: DigitizedSignal; "digitized_signal";
-        dimension: EISQ<Z0, Z0, Z0, Z0, Z0, Z0, Z0, P1, Z0>;
+        quantity: Digit; "digit";
+        dimension: CustomSQ<P1, Z0>;
         units {
-            @adc: prefix!(none); "adc", "adc", "adcs";
+            @adc : prefix!(none); "adc", "adc", "adcs";
             @kadc: prefix!(kilo); "kadc", "kadc", "kadcs";
         }
     }
+
+    use uom::si::ratio::Ratio;
+
+    impl<A, U, V> From<Ratio<A, V>> for Digit<U, V> where
+    A: uom::si::Units<V> + ?Sized,
+    U: super::Units<V> + ?Sized,
+    V: uom::num_traits::Num + uom::Conversion<V>,
+    {
+        fn from(val: Ratio<A, V>) -> Self {
+            Self {
+                dimension: uom::lib::marker::PhantomData,
+                units: uom::lib::marker::PhantomData,
+                value: val.value
+            }
+        }
+    }
+
 }
 
-// Define a new quantity
 #[macro_use]
-pub mod calibrated_signal {
+mod count {
     quantity! {
-        quantity: CalibratedSignal; "calibrated_signal";
-        dimension: EISQ<Z0, Z0, Z0, Z0, Z0, Z0, Z0, Z0, P1>;
+        quantity: Count; "count";
+        dimension: CustomSQ<Z0, P1>;
         units {
-            @pes: prefix!(none); "pes", "pes", "pes";
+            @pes : prefix!(none); "pes", "pes", "pes";
             @kpes: prefix!(kilo); "kpes", "kpes", "kpes";
+        }
+    }
+
+    use uom::si::ratio::Ratio;
+
+    impl<A, U, V> From<Ratio<A, V>> for Count<U, V> where
+    A: uom::si::Units<V> + ?Sized,
+    U: super::Units<V> + ?Sized,
+    V: uom::num_traits::Num + uom::Conversion<V>,
+    {
+        fn from(val: Ratio<A, V>) -> Count<U, V> {
+            Self {
+                dimension: uom::lib::marker::PhantomData,
+                units: uom::lib::marker::PhantomData,
+                value: val.value
+            }
         }
     }
 }
 
+
 system! {
-    quantities: EISQ {
-        length: meter, L;
-        mass: kilogram, M;
-        time: second, T;
-        electric_current: ampere, I;
-        thermodynamic_temperature: kelvin, Th;
-        amount_of_substance: mole, N;
-        luminous_intensity: candela, J;
-        digitized_signal: adc, Adc;
-        calibrated_signal: pes, Pes;
+    quantities: CustomSQ {
+        digit: adc, ADC;
+        count: pes, PES;
     }
 
-    units: ESIU {
-        // mod absement::Absement, // for some reason, it can't find this particular module
-        mod acceleration::Acceleration,
-        mod amount_of_substance::AmountOfSubstance,
-        mod angle::Angle,
-        mod angular_acceleration::AngularAcceleration,
-        mod angular_jerk::AngularJerk,
-        mod angular_velocity::AngularVelocity,
-        mod area::Area,
-        mod available_energy::AvailableEnergy,
-        mod capacitance::Capacitance,
-        mod catalytic_activity::CatalyticActivity,
-        mod catalytic_activity_concentration::CatalyticActivityConcentration,
-        mod curvature::Curvature,
-        mod electric_charge::ElectricCharge,
-        mod electric_current::ElectricCurrent,
-        mod electric_potential::ElectricPotential,
-        mod electrical_conductance::ElectricalConductance,
-        mod electrical_resistance::ElectricalResistance,
-        mod energy::Energy,
-        mod force::Force,
-        mod frequency::Frequency,
-        mod heat_capacity::HeatCapacity,
-        mod heat_flux_density::HeatFluxDensity,
-        mod heat_transfer::HeatTransfer,
-        mod inductance::Inductance,
-        mod information::Information,
-        mod information_rate::InformationRate,
-        mod jerk::Jerk,
-        mod length::Length,
-        mod luminance::Luminance,
-        mod luminous_intensity::LuminousIntensity,
-        mod magnetic_flux::MagneticFlux,
-        mod magnetic_flux_density::MagneticFluxDensity,
-        mod mass::Mass,
-        mod mass_concentration::MassConcentration,
-        mod mass_density::MassDensity,
-        mod mass_rate::MassRate,
-        mod molar_concentration::MolarConcentration,
-        mod molar_energy::MolarEnergy,
-        mod molar_heat_capacity::MolarHeatCapacity,
-        mod molar_mass::MolarMass,
-        mod momentum::Momentum,
-        mod power::Power,
-        mod pressure::Pressure,
-        mod ratio::Ratio,
-        mod specific_heat_capacity::SpecificHeatCapacity,
-        mod solid_angle::SolidAngle,
-        mod radiant_exposure::RadiantExposure,
-        mod temperature_interval::TemperatureInterval,
-        mod thermal_conductivity::ThermalConductivity,
-        mod thermodynamic_temperature::ThermodynamicTemperature,
-        mod time::Time,
-        mod torque::Torque,
-        mod velocity::Velocity,
-        mod volume::Volume,
-        mod volume_rate::VolumeRate,
-
-        mod digitized_signal::DigitizedSignal,
-        mod calibrated_signal::CalibratedSignal,
+    units: CustomSU {
+        mod digit::Digit,
+        mod count::Count,
     }
 }
 
 mod f32 {
-    use crate::ESIU;
-
-    mod extended_isq {
-        pub use super::*;
-        pub use super::super::si::*;
-        pub use super::super:: digitized_signal;
-        pub use super::super::calibrated_signal;
+    mod detector {
+        pub use super::super::*;
     }
 
-    EISQ!(self::extended_isq, f32);
+    CustomSQ!(self::detector, f32);
 }
 
-mod f64 {
-    use crate::ESIU;
 
-    mod extended_isq {
-        pub use super::*;
-        pub use super::super::si::*;
-        pub use super::super:: digitized_signal;
-        pub use super::super::calibrated_signal;
+mod f64 {
+    mod detector {
+        pub use super::super::*;
     }
 
-    EISQ!(self::extended_isq, f64);
+    CustomSQ!(self::detector, f64);
 }
 
 
 fn main() {
-    let some_adc = f32:: DigitizedSignal::new::<kadc>(42_f32);
-    let some_pes = f64::CalibratedSignal::new::<kpes>(PI);
+    let some_length  = Length::new::<millimeter>(9.8_f64);
+    let other_length = Length::new::<millimeter>(5.9_f64);
+    let some_time    = Time  ::new::<nanosecond>(1.1_f64);
+    let other_time   = Time  ::new::<nanosecond>(5.5_f64);
 
+    let some_length_ratio : Ratio = some_length / other_length;
+    let some_time_ratio   : Ratio = some_time   / other_time;
+
+    let some_adc = Digit::new::<kadc>(42_f64);
+    let some_pes = Count::new::<kpes>(PI);
+
+    let adc_from_ratio : Digit = Digit::from(some_time_ratio);
+    let pes_from_ratio : Count = Count::from(some_length_ratio);
+
+
+    // These don't
     #[cfg(feature = "compile-error")]
-    let calibration  = some_pes / some_adc; // Doesn't work out of the box
+    {
+        let _  = some_lengh + some_time;
+        let _  = some_lengh + some_adc;
+        let _  = some_lengh + some_pes;
 
-    println!("I have {}, which corresponds to {}",
+        let _  = some_adc + some_pes;
+    }
+
+    // This works
+    let _ = some_length / some_time;
+
+    // This doesn't work out of the box
+    #[cfg(feature = "compile-error")]
+    let calibration  = some_pes / some_adc;
+
+
+    println!("I have {} and {}",
         some_adc.into_format_args(adc, Abbreviation),
         some_pes.into_format_args(pes, Abbreviation)
     );
+
+    println!("From ratios I got {} and {}",
+        adc_from_ratio.into_format_args(adc, Abbreviation),
+        pes_from_ratio.into_format_args(pes, Abbreviation)
+    );
+
 }
